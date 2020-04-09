447548dd44a2628984b3f4aeba7740e60543aa1c
Payton Jewell <paytontaylor96@gmail.com>
2020-04-09 15:01:14 -0500

    removed empty variations file

--- variations.js
+++
@@ -1,7 +0,0 @@
-export const getNumberOfChars = (yourString) => name.length;
-
-export const getNumberOfCharacters = (yourString) => name.length;
-
-export const getFirstChar = (yourString) => name[0];
-
-export const getLastChar = (yourString) => name[name.length-1];
